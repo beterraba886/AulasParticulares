f460e23c6886aabba2d60edf02b0b9afb507064e
yama-senpai <rayanepaiiva@live.com>
2020-07-05 15:02:04 -0300

    documentação e remoção de comentarios

--- site/assets/js/home.js
+++ site/assets/js/home.js
@@ -2,17 +2,23 @@ $(document).ready()
 {
     //let teste  = localStorage.getItem('db_usuarios');
     let main = document.querySelector('main');
+
     let usuarioCorrente = JSON.parse(sessionStorage.getItem('usuarioCorrente'));
+
     let logout = document.querySelector('#logout').addEventListener('click', logoutUser);
+
     let data_atual = new Date();
 
-    //
+    usuarioCorrente.aulas_agendadas.forEach( (materia) => {        
 
-    usuarioCorrente.aulas_agendadas.forEach( (materia) => {
         materia.data_aula.forEach( (data_materia) =>{
+
             data_materia = new Date(data_materia);
-            console.log(data_materia);
+
+            //console.log(data_materia);
+            
             if((addDays(data_atual, -7) < data_materia) && (data_atual > data_materia)){
+
                 let cardDFlex = document.createElement('div');
                 cardDFlex.className = 'd-flex justify-content-center';                
                 cardDFlex.style.backgroundColor = '#ffffff';
@@ -26,35 +32,49 @@ $(document).ready()
 
                 let cardTitle = document.createElement('h4');
                 cardTitle.className = 'card-title';
+                cardTitle.id = 'disciplina';
                 cardTitle.style.fontFamily = 'Capriola, sans-serif';
-                cardTitle.innerText = materia.disciplina;//'Matemática';
+                cardTitle.innerText = materia.disciplina;
 
                 let textMuted1 = document.createElement('h6');
                 textMuted1.className = 'text-muted card-subtitle mb-2';
+                textMuted1.id = 'nome';
                 textMuted1.style.fontFamily = 'Capriola, sans-serif'; 
-                textMuted1.innerText = materia.professor//'Professor João';
+                textMuted1.innerText = materia.professor;
 
                 let textMuted2 = document.createElement('h6');                
                 textMuted2.className = 'text-muted card-subtitle mb-2';
+                textMuted2.id = 'data_hora';
                 textMuted2.style.fontFamily = 'Capriola, sans-serif';
                 textMuted2.innerText = 'Dia: 00/00/2020 - Hora: 00:00';
                 
                 let cardText = document.createElement('p');
                 cardText.className = 'card-text';
+                cardText.id = 'descricao';
                 cardText.style.fontFamily = 'Capriola, sans-serif';
                 cardText.innerText = 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.';
 
                 let button = document.createElement('button');
                 button.className = 'btn btn-primary';
-                cardText.style.fontFamily = 'Capriola, sans-serif';
+                button.id = 'btn_cancelar';
+                button.style.fontFamily = 'Capriola, sans-serif';
                 button.type = 'button';
                 button.innerText = 'Cancelar Agendamento';
+
+                let button2 = document.createElement('button');
+                button2.className = 'btn btn-primary';
+                button2.id = 'btn_adiar';
+                button2.style.fontFamily = 'Capriola, sans-serif';
+                button2.style.marginLeft = '10px';
+                button2.type = 'button';
+                button2.innerText = 'Adiar';
         
                 cardBody.appendChild(cardTitle);
                 cardBody.appendChild(textMuted1);
                 cardBody.appendChild(textMuted2);
                 cardBody.appendChild(cardText);
                 cardBody.appendChild(button);
+                cardBody.appendChild(button2);
 
                 card.appendChild(cardBody);                
 
@@ -65,53 +85,22 @@ $(document).ready()
 
         });
 
-
-        /*
-        let card = document.createElement('div');
-        card.className = 'card text-center';
-  
-        let cardHeader = document.createElement('div');
-        cardHeader.className = 'card-header';
-        cardHeader.innerText = materia.id;
-
-        let cardBody = document.createElement('div');
-        cardBody.className ='card-body';
-        
-        let cardBodyTitulo = document.createElement('h5');
-        cardBodyTitulo.className = "card-title";
-        cardBodyTitulo.innerText = materia.professor;
-
-        let cardBodyTexto = document.createElement('p');
-        cardBodyTexto.className = "card-text";
-        cardBodyTexto.innerText = materia.disciplina;
-
-        let cardBodyBotao = document.createElement('a');
-        cardBodyBotao.className = 'btn btn-primary';
-        cardBodyBotao.innerText = materia.valor;
-        cardBodyBotao.href = `materia.html?materia_id=${materia.id}`;        
-
-        cardBody.appendChild(cardBodyTitulo);
-        cardBody.appendChild(cardBodyTexto);
-        cardBody.appendChild(cardBodyBotao);
-        cardHeader.appendChild(cardBody);
-        card.appendChild(cardHeader);
-
-        main.appendChild(card);
-        */
         ;
     });
 
     // Apaga os dados do usuário corrente no sessionStorage
-        function logoutUser () {
-            usuarioCorrente = {};
-            sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
-            window.location = 'index.html';
-        }
+    function logoutUser () {
+
+        usuarioCorrente = {};
+        sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+        window.location = 'index.html';
+    }
 
     // Função para data
     function addDays(date, days) {
+
         var result = new Date(date);
         result.setDate(result.getDate() + days);
         return result;
-      }
+    }
 }
